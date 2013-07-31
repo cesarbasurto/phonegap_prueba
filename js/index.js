b561534b2487b0302e26ec681e67939b99194c27
Cesar Basurto <cesarbasurto7@gmail.com>
2013-07-31 16:44:19 -0500

    capturar

--- js/index.js
+++ js/index.js
@@ -1,80 +1,57 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        document.addEventListener('backbutton', this.exitAppPopup, false);
-		app.receivedEvent('deviceready');
-    },
-    
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-    	
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-		//alert("hola");
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+//*********************************************************
+		// Wait for Cordova to Load
+		//*********************************************************
 		
-        console.log('Received Event: ' + id);
-    },
-     exitAppPopup:function() {
-     	app.showAlert();
-	    /*navigator.notification.confirm(
-	          'Exit PhoneGap ' + device.cordova + ' Demo?'
-	        , function(button) {
-	              if (button == 2) {
-	                  navigator.app.exitApp();
-	              } 
-	          }
-	        , 'Exit'
-	        , 'No,Yes'
-	    );  */
-	    //return false;
-	},
-    alertDismissed:function() {
-            // do something
-    },
+	    var rootDir = "file:///mnt/sdcard";
+	    var appHomeDir = "Android/data/com.ventusmoso.pg2_jqm";
+	
+	    //*********************************************************    
+	    // Prepare dir for file and storage features
+	    //*********************************************************  
+	    function createFileDir() {
+	
+	        function fsOnError(error) {
+	            alert("Cannot get to root file system. Code: " + error.message)
+	        }
+	        function dirOnError(error) {
+	            alert("Cannot create dir. Code: " + error.code)
+	        }
+	        function fsOnSuccess(fileSystem) {
+	            fileSystem.root.getDirectory(appHomeDir, {create: true, exclusive: false}, null, dirOnError);
+	        }
+	
+	        window.requestFileSystem( LocalFileSystem.PERSISTENT, 0, fsOnSuccess, fsOnError );
+	    }
 
-    // Show a custom alertDismissed
-    //
-    showAlert:function() {
-        navigator.notification.alert(
-            'You are the winner!',  // message
-            app.alertDismissed,         // callback
-            'Game Over',            // title
-            'Done'                  // buttonName
-        );
-    }
-    
-};
+
+    	document.addEventListener("deviceready", onDeviceReady, false);
+    	function onDeviceReady() {
+			 $("#main h1#appTitle").html("PhoneGap " + device.cordova + " Demo");       
+	       // addBackbuttonListener();
+	        createFileDir();
+			
+		}
+		
+		document.addEventListener("backbutton", function() {
+            if ( $('.ui-page-active').attr('id') == 'main') {
+                exitAppPopup();
+            } else {
+                history.back();             
+            }
+        }, false);
+        
+        function exitAppPopup() {
+		    navigator.notification.confirm(
+		          'Exit PhoneGap ' + device.cordova + ' Demo?'
+		        , function(button) {
+		              if (button == 2) {
+		                  navigator.app.exitApp();
+		              } 
+		          }
+		        , 'Exit'
+		        , 'No,Yes'
+		    );  
+		    return false;
+		}
+		
+		
