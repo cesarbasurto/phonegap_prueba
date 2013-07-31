d76957b8528b6e425151ad0e87fd26336ca0fcf9
Cesar Basurto <cesarbasurto7@gmail.com>
2013-07-31 10:29:16 -0500

    hoka

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ var app = {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
+		alert("hola");
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
